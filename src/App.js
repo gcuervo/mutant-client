585993cf9a891c6a8cace4a43dd25fca7895d884
Gaston <cuervogaston@gmail.com>
2018-09-24 01:28:43 -0300

    Save and get stats

--- src/App.js
+++ src/App.js
@@ -6,14 +6,54 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      ratio: 0.0,
+      human: 0,
+      mutant: 0
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getStats();
   }
 
   handleChange(event) {
     this.setState({ value: event.target.value });
+  }
+
+  saveDna(dna, isMutant) {
+    fetch('http://localhost:8090/mutant/dna', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        dna: dna,
+        mutant: isMutant
+      })
+    }).then(res => res.json())
+      .then(res => console.log(res));
+  }
+
+  getStats() {
+    fetch('http://localhost:8090/mutant/stats', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(res => res.json())
+      .then(res => {
+        this.setState({ value: this.state.value,
+          ratio: res.ratio,
+          mutant: res.count_mutant_dna,
+          human: res.count_human_dna});
+        console.log(res);
+      });
   }
 
   handleSubmit(event) {
@@ -31,7 +71,12 @@ class App extends Component {
         dna: dna
       })
     }).then(res => res.json())
-      .then(res => console.log(res));
+      .then(res => {
+        console.log(res.mutant);
+        //console.log(res.mutant);
+        this.saveDna(dna, res.mutant);
+        this.getStats();
+      });
 
   }
 
@@ -40,7 +85,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Am i a mutant?</h1>
         </header>
 
         <div className="row">
@@ -50,6 +95,29 @@ class App extends Component {
                 <button className="btn btn-outline-secondary" onClick={this.handleSubmit} type="button">Send</button>
               </div>
               <input type="text" className="form-control" placeholder="Insert DNA" value={this.state.value} onChange={this.handleChange} aria-describedby="basic-addon1" />
+            </div>
+          </div>
+        </div>
+
+        <div className="mutant-table">
+          <div className="row">
+            <div className="col-lg-5 col-centered col-lg-offset-4">
+              <table className="table ">
+                <thead className="thead-dark">
+                  <tr>
+                    <th scope="col">Humanos</th>
+                    <th scope="col">Mutantes</th>
+                    <th scope="col">Ratio</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{this.state.human}</td>
+                    <td>{this.state.mutant}</td>
+                    <td>{this.state.ratio}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
